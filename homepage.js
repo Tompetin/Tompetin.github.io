960573595e882a3ff29351adb9a735136f35f9d9
Tommi Salo <103764166+Tompetin@users.noreply.github.com>
2024-11-24 18:50:53 +0100

    test

--- homepage.js
+++ homepage.js
@@ -1,4 +1,4 @@
 function startGame() {
     // Redirect the user to the game levels page
-    window.location.href = "/levels/level1/level1.html"; // Replace with the actual file path to the first level.
+    window.location.href = "levels/level1/level1.html"; // Replace with the actual file path to the first level.
 }
